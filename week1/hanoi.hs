f7d37ef41ae5fff5ab02581edd37d8fb15653b1c
yinhail <yinhail@gmail.com>
2014-06-25 21:33:57 +0800

    hanoi works

--- week1/hanoi.hs
+++ week1/hanoi.hs
@@ -1,12 +1,8 @@
-type Peg = String 
+type Peg = Int 
 type Move = (Peg, Peg)
 
-hanoi :: Integer -> Peg -> Peg -> [Move]
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 
-hanoi n "A" "B" = (hanoi (n-1) "A" "C") ++ [("A", "B")] ++ (hanoi (n-1) "B" "C")
-hanoi n "A" "C" = (hanoi (n-1) "A" "B") ++ [("A", "C")] ++ (hanoi(n-1) "C" "B")
-hanoi n "B" "A" = (hanoi (n-1) "B" "C") ++ [("B", "A")] ++ (hanoi (n-1) "C" "A")
-hanoi n "B" "C" = (hanoi (n-1) "B" "A") ++ [("B", "C")] ++ (hanoi (n-1) "A" "C")
-hanoi n "C" "B" = (hanoi (n-1) "C" "A") ++ [("C", "B")] ++ (hanoi (n-1) "A" "B")
-hanoi n "C" "A" = (hanoi (n-1) "C" "B") ++ [("C", "A")] ++ (hanoi (n-1) "B" "A")
-hanoi 0 _ _ = []
+--the sequence of the following two lines are important
+hanoi 0 from to using = []
+hanoi n from to using = hanoi (n-1) from using to ++ [(from, to)] ++ hanoi (n-1) using to from
